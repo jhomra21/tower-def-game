8822d8ae3a17cf4bc9764203a8bb312cb610a54a
Juan <jhonra@icloud.com>
2025-01-12 01:18:51 -0600

    fix swc missing lol

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          'three': ['three']
+          'three': ['three'],
         }
       }
     }
